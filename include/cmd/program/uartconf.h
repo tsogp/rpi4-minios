e25f71ba4553608f931b24568bbf8f0b8a1e0dfa
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-03-10 22:15:46 +0700

    fix uartconf description

--- include/cmd/program/uartconf.h
+++ include/cmd/program/uartconf.h
@@ -18,7 +18,7 @@
         " -p [PARITY_CONFIG]     Sets the parity configuration. Only available options of\n"\
         "                        PARITY_CONFIG are NONE, EVEN or ODD.\n"      \
         " -h [PROTOCOL]          Sets the handshaking configuration. Only available options of\n"\
-        "                        PROTOCOL are CTS and RTS.\n"               \
+        "                        PROTOCOL are ENABLED or DISABLED.\n"               \
         "Example:\n"  \
         "  uartconf -b 19200                set baud rate to 9600\n" \
         "  uartconf -d 6 -s 2 -p NONE       set the number of data bits to 6, enable 2 stop bits and disable parity\n"
